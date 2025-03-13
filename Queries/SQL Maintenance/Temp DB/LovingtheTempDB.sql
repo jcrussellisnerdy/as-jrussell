986315063bf47054f8c420e67fa7f306dbcaafe8
as-jrussell <joseph.russell@alliedsolutions.net>
2025-03-13 06:34:15 -0400

    Update LovingtheTempDB.sql

--- Queries/SQL Maintenance/Temp DB/LovingtheTempDB.sql
+++ Queries/SQL Maintenance/Temp DB/LovingtheTempDB.sql
@@ -8,11 +8,11 @@ DECLARE @GROWTH INT
 DECLARE @ISPERCENT INT
 DECLARE @LogFile NVARCHAR(10) 
 DECLARE @Percentage DECIMAL(5,2) = 0.20;
-DECLARE @ServerLocation NVARCHAR(10) = 'AWS-EC2'
+DECLARE @ServerLocation NVARCHAR(10) = ''
 
 SELECT @LogFile = CASE WHEN ServerLocation = @ServerLocation AND ServerEnvironment NOT IN ('PRD','PROD', 'ADMIN' ,'ADM') THEN '1' 
 WHEN ServerLocation = @ServerLocation AND ServerEnvironment  IN ('PRD','PROD', 'ADMIN' ,'ADM') THEN '10' 
-ELSE 0 END
+ELSE 1 END
 from dba.info.Instance
 
 
@@ -51,20 +51,22 @@ IF @ISPERCENT = 0 SET @GROWTH = @GROWTH * 8
 SELECT 
     (m.size * 8) / 1024 AS [Starting size (MB)],
     (d.size * 8) / 1024 AS [Current size (MB)], 
-	I.SQlServername,
-	ServerLocation,
-	ServerEnvironment, 
+	ISNULL(I.SQlServername,@@SERVERNAME) [InstanceName],
+	ISNULL(ServerLocation,'Research')[InstanceLocation],
+	ISNULL(ServerEnvironment,'Research')[InstanceEnvironment],
 	CASE WHEN CAST(@CORES AS NVARCHAR(100)) > CAST(@FILECOUNT AS NVARCHAR(100)) THEN 
 	'Server '+ I.SQlServername +' needs ' + CAST(@CORES AS NVARCHAR(100)) + ' TempDB data files, now there is ' + CAST(@FILECOUNT AS NVARCHAR(100)) + CHAR(10) + CHAR(13)
 	ELSE 'No datafile growth needed' END
-	[Data File Needed],
-ROUND(CONVERT(DECIMAL(18,2), vs.total_bytes/1073741824.0),0) AS [Total Size (GB)],
+	[Data File Needed], ISNULL(
+ROUND(CONVERT(DECIMAL(18,2), vs.total_bytes/1073741824.0),0),'0') AS [Total Size (GB)],
+ISNULL(
 ROUND(
     (CONVERT(DECIMAL(18,2), vs.total_bytes / 1073741824.0)  
      - CONVERT(DECIMAL(18,2), (vs.total_bytes / 1073741824.0) * @Percentage) 
      - CONVERT(DECIMAL(18,2), @LogFile)
-    ), 0) [Usable space for drive],
-
+    ), 0), '0')
+	[Usable space for drive],
+ISNULL(
 ROUND(
     CONVERT(DECIMAL(18,2), 
         ( 
@@ -73,18 +75,18 @@ ROUND(
              - CONVERT(DECIMAL(18,2), @LogFile)
             ) 
         / CAST(@CORES AS DECIMAL(18,2))  
-    ), 0),0) 
+    ), 0),0), '0' )
 [Amount per datafile]
 ,CASE WHEN ServerLocation = 'AWS-EC2' AND ServerEnvironment NOT IN ('PRD','PROD', 'ADMIN' ,'ADM') THEN @LogFile+' GB' 
 WHEN ServerLocation = 'AWS-EC2' AND ServerEnvironment  IN ('PRD','PROD', 'ADMIN' ,'ADM') THEN @LogFile+' GB'
 WHEN ServerLocation = 'AWS-RDS' THEN 'N/A'
 ELSE 'Research'
 END [Log File]
-
+--SELECT *
 FROM sys.master_files AS m
 JOIN tempdb.sys.database_files AS d ON m.file_id = d.file_id
 CROSS APPLY sys.dm_os_volume_stats(m.database_id, m.[file_id]) AS vs 
-cross join DBA.INFO.INSTANCE i
+OUTER APPLY DBA.INFO.INSTANCE i
 WHERE DB_NAME(m.database_id) = 'tempdb'
 AND  m.type_desc = 'LOG'
 AND  (  (d.size * 8) / 1024 ) <= (@LogFile * 1024)
